07690e629b5453babb107cd823773f8eb49c6b78
Sefa <sefayilmazkurumsal@gmail.com>
2022-08-14 20:39:05 +0300

    IFinish Refactor

--- Assets/[Game]/Scripts/MonoObjects/EndGameOnCollision.cs
+++ Assets/[Game]/Scripts/MonoObjects/EndGameOnCollision.cs
@@ -1,13 +1,23 @@
 using UnityEngine;
 
-public class EndGameOnCollision : MonoBehaviour
+public class EndGameOnCollision : MonoBehaviour, IFinish
 {
     private void OnTriggerEnter(Collider other)
     {
+        FinishGame(other);
+    }
+    
+    public void FinishGame(Collider other)
+    {
         var ragdoll = other.GetComponentInParent<Ragdoll>();
-        if(!ragdoll)
+        if (!ragdoll)
             return;
         GetComponent<Collider>().enabled = false;
         GameManager.Instance.CompleteLevel();
     }
+}
+
+public interface IFinish
+{
+    void FinishGame(Collider other);
 }
